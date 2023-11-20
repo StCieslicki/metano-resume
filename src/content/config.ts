64c70def31dcb036a75e5e27cb188f8624cd8136
Stanisław Cieślicki <Stanislaw.Cieslicki@ringieraxelspringer.pl>
2023-11-20 15:58:09 +0100

    add language translations

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,6 +8,7 @@ const projectsCollection = defineCollection({
 		tags: z.array(z.string()),
 		link: z.string(),
 		img_alt: z.string().optional(),
+		lang: z.string().optional(),
 	}),
 });
 
--- src/content/projects/en/project-3.md
+++ src/content/projects/en/project-3.md
@@ -5,4 +5,5 @@ description: Project description
 img_alt: project image alt text
 link: https://github.com/veranikabarel?tab=repositories
 tags: ['React', 'CSS', 'Typescript']
+lang: en
 ---
