830deaa9d8da304ca257a7c73ac71655e25cf415
Stanisław Cieślicki <Stanislaw.Cieslicki@ringieraxelspringer.pl>
2023-11-26 13:43:32 +0100

    add robots.txt generator add Links.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,29 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
-	srcDir: './src',
-	publicDir: './public',
-	outDir: './build',
+  site: 'https://resume.metano.pl',
 
-	integrations: [tailwind(), mdx(), image()],
+  srcDir: './src',
+  publicDir: './public',
+  outDir: './build',
+  integrations: [tailwind(), mdx(), image(), robotsTxt(
+      {
+        sitemap: false,
+          policy: [
+              {
+                  disallow: [
+                      '/download/*',
+                      '/resume/*',
+                      '/cover-letter/*',
+                      '/extended/*',
+                      '/print/*',
+                  ]
+              }
+          ]
+      },
+  )]
 });
